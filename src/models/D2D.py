67ef5bcff7a7eed27ddaf42018230d796213fa51
zhengzx <zhengzx@nlp.nju.edu.cn>
2019-12-04 01:27:05 +0000

    [refactor] [add] add seg-rel attn for glb layer

--- src/models/D2D.py
+++ src/models/D2D.py
@@ -15,33 +15,59 @@ from src.models import transformer
 from src.models.mem_transformer import MemTransformerLM
 from src.modules.transformer_xl_utils.parameter_init import weights_init
 from src.modules.embeddings import Embeddings
-from src.modules.position_embedding import PositionalEmbedding, SegmentEmbedding
+from src.modules.position_embedding import PositionalEmbedding, SegmentEmbedding, RelativePositionEmbeddings, RelativeSegmentEmbeddings
 from src.decoding.utils import tile_batch, tensor_gather_helper
 from src.utils import nest
 from src.modules.sublayers import MultiHeadedAttention, PositionwiseFeedForward
+from src.modules.relative_attention import MultiHeadedAttentionRelative
 from src.data.vocabulary import PAD, EOS, BOS
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, d_model, d_inner_hid, n_head, dim_per_head, dropout=0.1, **kwargs):
+    attention_cls = {
+        "normal": MultiHeadedAttention,
+        "relative": MultiHeadedAttentionRelative
+    }
+    
+    def __init__(self, d_model, d_inner_hid, n_head, dim_per_head, dropout=0.1, attention_type="normal", **kwargs):
         super().__init__()
         self.layer_norm = nn.LayerNorm(d_model)
-        self.slf_attn = MultiHeadedAttention(head_count=n_head, model_dim=d_model, dropout=dropout,
-                                             dim_per_head=dim_per_head)
+
+        attn_cls = EncoderBlock.attention_cls[attention_type]
+        self.slf_attn = attn_cls(head_count=n_head, model_dim=d_model, dropout=dropout,
+                                 dim_per_head=dim_per_head)
         self.pos_ffn = PositionwiseFeedForward(size=d_model, hidden_size=d_inner_hid, dropout=dropout)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, enc_input, slf_attn_mask=None):
+    def forward(self, enc_input, slf_attn_mask=None, rel_attn_kv=(None, None)):
         input_norm = self.layer_norm(enc_input)
-        context, _, _ = self.slf_attn(input_norm, input_norm, input_norm, slf_attn_mask)
+        context, _, _ = self.slf_attn(input_norm, input_norm, input_norm, slf_attn_mask, rel_attn_kv=rel_attn_kv)
         out = self.dropout(context) + enc_input
         return self.pos_ffn(out)
 
 
-class Encoder(transformer.Encoder):
-
+class Encoder(nn.Module):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__()
+        
+        self._build_embeddings(**kwargs)
+
+        self._build_encoder_layers(**kwargs)
+
+        self._build_global_encoder_layer(**kwargs)
+        
+        self.layer_norm = nn.LayerNorm(kwargs["d_model"])
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        scale = self.embeddings.scale
+        nn.init.normal_(self.embeddings.embeddings.weight,
+                        mean=0, std=1/scale)
+        nn.init.constant_(self.embeddings.embeddings.weight[self.embeddings.padding_idx], 0)
+
+    # -------------------------- builds -------------------------------- #
+    def _build_embeddings(self, **kwargs):
         self.embeddings = Embeddings(num_embeddings=kwargs["n_src_vocab"],
                                      embedding_dim=kwargs["d_word_vec"],
                                      dropout=False,
@@ -54,23 +80,44 @@ class Encoder(transformer.Encoder):
             max_segment=kwargs["max_encoder_segment_embedding"]) \
             if kwargs["max_encoder_segment_embedding"] > 0 \
             else None
+    
+    def _build_encoder_layers(self, **kwargs):
+        attn_type = kwargs.get("encoder_attention_type", "normal")
+        self.attn_type = attn_type
 
-        self.mix_local_global = kwargs.get("mix_encoder_local_global_attention", False)
-        if self.mix_local_global:
+        if attn_type == "relative":
+            self.block_stack = nn.ModuleList(
+                [EncoderBlock(attention_type="relative", **kwargs) for _ in range(kwargs["n_layers"])])
+            self.rel_pos_key = RelativePositionEmbeddings(128, kwargs["d_model"]//kwargs["n_head"])
+            self.rel_pos_values = RelativePositionEmbeddings(128, kwargs["d_model"]//kwargs["n_head"])
+
+        elif attn_type == "normal":
+            self.block_stack = nn.ModuleList(
+                [EncoderBlock(**kwargs) for _ in range(kwargs["n_layers"])]) 
+            self.rel_pos_key, self.rel_pos_values = None, None
+            
+    def _build_global_encoder_layer(self, **kwargs):
+        glb_attn_type = kwargs.get("global_encoder_attention_type", "none")
+        self.glb_attn_type = glb_attn_type
+        if glb_attn_type == "none":
+            self.global_encoder_layer = None
+
+        elif glb_attn_type == "normal":
             self.global_encoder_layer = EncoderBlock(**kwargs)
 
-        self.reset_params()
+        elif glb_attn_type == "word-relative":
+            raise NotImplementedError
 
-    def reset_params(self):
-        scale = self.embeddings.scale
-        nn.init.normal_(self.embeddings.embeddings.weight,
-                        mean=0, std=1/scale)
-        nn.init.constant_(self.embeddings.embeddings.weight[self.embeddings.padding_idx], 0)
-        if self.segment_embed is not None:
-            nn.init.normal_(self.segment_embed.embed.weight,
-                            mean=0, std=1/scale)
+        elif glb_attn_type == "segment-relative":
+            self.global_encoder_layer = EncoderBlock(
+                attention_type="relative", **kwargs)
+            self.global_rel_seg_k = RelativeSegmentEmbeddings(
+                kwargs["max_encoder_segment_embedding"], kwargs["d_model"]//kwargs["n_head"])
+            self.global_rel_seg_v = RelativeSegmentEmbeddings(
+                kwargs["max_encoder_segment_embedding"], kwargs["d_model"]//kwargs["n_head"])
 
-    def build_local_mask(self, segment_ids, enc_mask):
+    # -------------------------- prepares -------------------------------- #
+    def _prepare_local_mask(self, segment_ids, enc_mask):
         # segment_ids: [bsz, len]. [[0,0,0,1,1,1,2,2,2,2...], ...]
         # enc_mask: [bsz, qlen, mlen] (qlen==mlen)
         # return: [bsz, qlen, mlen] (qlen==mlen). [[[0,0,0,1,1,1,1,1,1,1...], [1,1,1,0,0,0,1,1,1,1...],...]]
@@ -85,34 +132,111 @@ class Encoder(transformer.Encoder):
         local_mask = torch.stack(local_mask, 1)
         return (local_mask + enc_mask).gt(0)
 
+    def _prepare_masks(self, enc_mask, segment_ids):
+        # enc_mask: padding mask [bsz, src_len]
+        global_mask = enc_mask[:, None, :].expand(*enc_mask.size(), enc_mask.size(1))
+        
+        if self.glb_attn_type != "none": # which means the encoder layers using local encoder
+            local_mask = self._prepare_local_mask(segment_ids, global_mask)
+            encoder_self_attention_mask = local_mask
+            return encoder_self_attention_mask, global_mask
+
+        return global_mask, global_mask
+
+    def _prepare_relative(self, length):
+        # relative attention related
+        return self.rel_pos_key(length), self.rel_pos_values(length)
+
+    def _prepare_segment_relative(self, segment_ids):
+        # segment-level relative attention related
+        return self.global_rel_seg_k(segment_ids), self.global_rel_seg_v(segment_ids)
+
+    # -------------------------- forwards -------------------------------- #
+    def forward_embedding(self, src_seq, position=None, segment_ids=None):
+        emb = self.embeddings(src_seq)
+        if self.attn_type == "normal":
+            # segment embedding
+            if segment_ids is not None and self.segment_embed is not None:
+                seg_emb = self.segment_embed(segment_ids)
+                emb = emb + seg_emb
+            # absolute position embeddings. 
+            # reset for each segment or not
+            if self.reset_position and position is not None:
+                emb = self.pos_emb(emb, pos_seq=position) 
+            else:
+                emb = self.pos_emb(emb)
+            return emb
+
+        elif self.attn_type == "relative":
+            # do not use absolute pe when using relative attention
+            return emb
+        
+    def forward_encoder_layers(self, emb, encoder_layer_mask):
+        out = emb
+
+        if self.attn_type == "normal":
+            for i in range(self.num_layers):
+                out = self.block_stack[i](
+                    out,
+                    encoder_layer_mask)
+
+        elif self.attn_type == "relative":
+            # relative attention related
+            src_len = emb.size(1)
+            rel_k, rel_v = self._prepare_relative(src_len)
+            for i in range(self.num_layers):
+                out = self.block_stack[i](
+                    out,
+                    encoder_layer_mask,
+                    rel_attn_kv=[rel_k, rel_v])
+        else:
+            raise NotImplementedError
+        return out
+
+    def forward_global_encoder_layer(self, out, global_encoder_mask, segment_ids):
+        if self.glb_attn_type == "none":
+            return out
+
+        elif self.glb_attn_type == "normal":
+            return self.global_encoder_layer(out, global_encoder_mask)
+
+        elif self.glb_attn_type == "word-relative":
+            raise NotImplementedError
+
+        elif self.glb_attn_type == "segment-relative":
+            seg_rel_k, seg_rel_v = self._prepare_segment_relative(segment_ids)
+            return self.global_encoder_layer(out, global_encoder_mask, rel_attn_kv=[seg_rel_k, seg_rel_v])
+
     def forward(self, src_seq, position=None, segment_ids=None):
         # Word embedding look up
-        batch_size, src_len = src_seq.size()
-
-        emb = self.embeddings(src_seq)
-        if segment_ids is not None and self.segment_embed is not None:
-            seg_emb = self.segment_embed(segment_ids)
-            emb += seg_emb
-        emb = self.pos_emb(emb, pos_seq=position if self.reset_position else None)
-
         enc_mask = src_seq.detach().eq(PAD)
-        enc_slf_attn_mask = enc_mask.unsqueeze(1).expand(batch_size, src_len, src_len)
-        if self.mix_local_global:
-            local_self_attn_mask = self.build_local_mask(segment_ids, enc_slf_attn_mask)
 
-        out = emb
-        for i in range(self.num_layers):
-            out = self.block_stack[i](
-                out,
-                enc_slf_attn_mask if not self.mix_local_global else local_self_attn_mask)
+        # embeddings
+        emb = self.forward_embedding(
+            src_seq=src_seq,
+            position=position,
+            segment_ids=segment_ids)
 
-        if self.mix_local_global:
-            out = self.global_encoder_layer(out, enc_slf_attn_mask)
+        # masks
+        encoder_self_attention_mask, global_attention_mask = \
+            self._prepare_masks(
+                enc_mask=enc_mask,
+                segment_ids=segment_ids)
 
-        out = self.layer_norm(out)
-        return out, enc_mask
-    
-    
+        # main encoder layers
+        out = self.forward_encoder_layers(
+            emb=emb,
+            encoder_layer_mask=encoder_self_attention_mask)
+
+        # global encoder layer, if applicable
+        out = self.forward_global_encoder_layer(
+            out=out,
+            global_encoder_mask=global_attention_mask,
+            segment_ids=segment_ids)
+
+        return self.layer_norm(out), enc_mask
+
+
 class D2D(NMTModel):
     def __init__(
             self, n_src_vocab, n_tgt_vocab, n_layers=6, n_head=8,
